36f24ca98ba84132df08dfe4fe2a29e293d3acad
Ulili Nhaga <3706665+ulili5@users.noreply.github.com>
2024-02-15 11:04:41 -0800

    set files max_line_length to 120

--- src/infra/eks-apps/src/main.ts
+++ src/infra/eks-apps/src/main.ts
@@ -1,6 +1,6 @@
-import { Construct } from "constructs";
-import { App, Chart, ChartProps } from "cdk8s";
-import * as kplus from "cdk8s-plus-27";
+import {Construct} from 'constructs';
+import {App, Chart, ChartProps} from 'cdk8s';
+import * as kplus from 'cdk8s-plus-27';
 
 export interface SimpleWebApiChartProps extends ChartProps {
   image: string;
@@ -16,13 +16,13 @@ export default class SimpleWebApiChart extends Chart {
     scope: Construct,
     id: string,
     props: SimpleWebApiChartProps = {
-      image: "public.ecr.aws/ecs-sample-image/amazon-ecs-sample:latest",
+      image: 'public.ecr.aws/ecs-sample-image/amazon-ecs-sample:latest',
       portNumber: 80,
     }
   ) {
     super(scope, id, props);
 
-    const frontendApp = new kplus.Deployment(this, "web-api", {
+    const frontendApp = new kplus.Deployment(this, 'web-api', {
       containers: [
         {
           image: props.image,
@@ -42,25 +42,25 @@ export default class SimpleWebApiChart extends Chart {
       serviceType: kplus.ServiceType.CLUSTER_IP,
     });
 
-    const ingress = new kplus.Ingress(this, "ingress");
+    const ingress = new kplus.Ingress(this, 'ingress');
     ingress.addRule(`/api/${id}`, kplus.IngressBackend.fromService(frontendServices));
-    ingress.metadata.addAnnotation("kubernetes.io/ingress.class", "alb");
-    ingress.metadata.addAnnotation("alb.ingress.kubernetes.io/scheme", "internet-facing");
-    ingress.metadata.addAnnotation("alb.ingress.kubernetes.io/listen-ports", '[{"HTTP": 80}]');
-    ingress.metadata.addAnnotation("alb.ingress.kubernetes.io/healthcheck-path", props.healthcheckPath ?? "/");
-    ingress.metadata.addAnnotation("alb.ingress.kubernetes.io/target-type", "ip");
+    ingress.metadata.addAnnotation('kubernetes.io/ingress.class', 'alb');
+    ingress.metadata.addAnnotation('alb.ingress.kubernetes.io/scheme', 'internet-facing');
+    ingress.metadata.addAnnotation('alb.ingress.kubernetes.io/listen-ports', '[{"HTTP": 80}]');
+    ingress.metadata.addAnnotation('alb.ingress.kubernetes.io/healthcheck-path', props.healthcheckPath ?? '/');
+    ingress.metadata.addAnnotation('alb.ingress.kubernetes.io/target-type', 'ip');
   }
 }
 
 const app = new App();
 
-const SIMPLE_API_CONTAINER_IMG = process.env.SIMPLE_API_CONTAINER_IMG ?? "";
-new SimpleWebApiChart(app, "app1", {
+const SIMPLE_API_CONTAINER_IMG = process.env.SIMPLE_API_CONTAINER_IMG ?? '';
+new SimpleWebApiChart(app, 'app1', {
   image: SIMPLE_API_CONTAINER_IMG,
   portNumber: 8080,
-  healthcheckPath: "/healthz",
+  healthcheckPath: '/healthz',
   envVariables: {
-    ASPNETCORE_ENVIRONMENT: kplus.EnvValue.fromValue("Development"),
+    ASPNETCORE_ENVIRONMENT: kplus.EnvValue.fromValue('Development'),
   },
 });
 
