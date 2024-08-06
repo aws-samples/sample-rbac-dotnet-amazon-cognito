19340ff9f6c265ab6c1b420f82feea9962a4a482
Farre, Carlos <cpfarre@amazon.com>
2024-08-06 14:53:35 -0700

    fixed api

---
+++ src/infra2/cognito/lib/main.ts
@@ -0,0 +1,54 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { CognitoStack } from "./cognito";
+import { IamStack } from "./iam";
+import { CognitoRoleMappingsStack } from "./cognitorolemappings";
+
+export class SecurityStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const region = cdk.Aws.REGION;
+
+    const app = new cdk.App();
+
+   /*  const iamStack: IamStack = new IamStack(app, "iamStack", {
+      env: { region: region },
+    }); */
+
+
+    
+
+    const cognitoStack: CognitoStack = new CognitoStack(app, "congnitoStack", {
+      env: { region: region },
+    });
+
+    const iamStack: IamStack = new IamStack(app, "iamStack", {
+      env: { region: region },
+    });
+
+    const cognitorolemappings: CognitoRoleMappingsStack = new CognitoRoleMappingsStack(
+      app,
+      "cognitoRoleMappingsStack",
+      {
+        env: { region: region },
+      }
+    );
+
+    iamStack.addDependency(cognitoStack);
+    cognitorolemappings.addDependency(iamStack);
+
+    
+    
+
+
+/* 
+    const cognitoStack: CognitoStack = new CognitoStack(app, "congnitoStack", {
+      env: { region: region },
+    });
+ */
+   // cognitoStack.addDependency(iamStack);
+
+  
+  }
+}
